8aabf8b1cc0e0bd5947d285f244c4a5173fa7e60
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-10-20 17:41:48 -0600

    Another major overhaul
     (1) 3 sections: implemented, testing, draft
     (2) overwrite 'document/archive' structure with 'link/file' structure
     (3) delete document_theme; confine themes to per-user setting
     (4) changed 'Actions' to 'Events/EventLogs'
     (5) delete event logging altogether
     (6) simplify 'Access' table into 'Permisions'

--- db.sql
+++ db.sql
@@ -1,73 +1,71 @@
-/* untested, work-in-progress */
+/* IMPLEMENTED - BEGIN */
+
+/* IMPLEMENTED - END */
+
+
+
+
+
+
+/* TESTING - BEGIN */
 
 CREATE TABLE Users (
-	user_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	user_id INT NOT NULL AUTO_INCREMENT,
+	user_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /* creation of user account */
+	user_id 			INT NOT NULL AUTO_INCREMENT,
 	
-	user_salt VARCHAR(255) NOT NULL,
-	user_hash VARCHAR(255) NOT NULL,
+	user_salt 			VARCHAR(255) NOT NULL, /* local .JS hash with email salt to send secure over http */
+	user_hash 			VARCHAR(255) NOT NULL, /* password, after processing */
 	
-	user_alias VARCHAR(255) NOT NULL UNIQUE,
-	user_email VARCHAR(255) NOT NULL UNIQUE,
-	user_image VARCHAR(255) NOT NULL DEFAULT '../users/_default.png',
+	user_alias			 VARCHAR(255) NOT NULL UNIQUE,
+	user_email 			VARCHAR(255) NOT NULL UNIQUE,
+	user_image 			VARCHAR(255) NOT NULL DEFAULT '../users/_default.png',
 	
 	PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Documents (
-	document_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	document_id INT NOT NULL AUTO_INCREMENT,
-	document_urlhash VARCHAR(255) NOT NULL UNIQUE,
-	document_user_lock INT DEFAULT null,
-	document_format INT NOT NULL DEFAULT 0,
+/* TESTING - END */
 
-	document_version INT NOT NULL DEFAULT 0,
 
-	document_title VARCHAR(255) NOT NULL DEFAULT '',
-	document_access byte NOT NULL DEFAULT 0, /* (0:private 1:protected 2:unlisted 3:public)  */
-	document_content TEXT NOT NULL, /* 64KB of storage for our JSON document object */
-	/* document_options VARCHAR(255) NOT NULL, */ /* I dislike the 'options' approach. Better to make explicit if needed. */
 
-	PRIMARY KEY (document_urlhash),
-	FOREIGN KEY (document_user_lock) REFERENCES Users(user_id),
-	FOREIGN KEY (document_format) REFERENCES Formats(format_id)
+
+
+
+/* DRAFT - BEGIN */
+
+CREATE TABLE Links (
+	link_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	link_id 			INT NOT NULL AUTO_INCREMENT,
+	link_path 			VARCHAR(255) NOT NULL UNIQUE, 			/* try {random hash} catch(collision) {repeat} */
+	
+	link_owner 			INT NOT NULL,
+	
+	link_hash 			VARCHAR(255) NOT NULL,
+	link_editor 			INT NOT NULL, 					/* on insert, set to owner */
+	link_version 			INT NOT NULL,
+	
+	PRIMARY KEY (link_id),
+	FOREIGN KEY (link_last_lock) 	REFERENCES Users(user_id),
+	FOREIGN KEY (link_last_lock) 	REFERENCES Users(user_id),
 );
 
-CREATE TABLE Formats (
-	format_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	format_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Files (
+	file_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	file_id 			INT NOT NULL AUTO_INCREMENT,
+	file_path 			VARCHAR(255) NOT NULL,
+	file_parent_link		INT NOT NULL,
 	
-	format_title VARCHAR(255) NOT NULL,
-	format_fontfamily VARCHAR(255) NOT NULL DEFAULT 'Arial',
-	format_fontsize INT NOT NULL DEFAULT 11, /* at first, no support for decimal/float font sizes */
-	format_background VARCHAR(255) NOT NULL DEFAULT 'linen', /* preliminary plan is to store as CSS interpretable value, either #000000 or color */
-	format_textcolour VARCHAR(255) NOT NULL DEFAULT 'black',
+	file_title 			VARCHAR(255) NOT NULL DEFAULT '',
+	file_privacy 			BYTE NOT NULL, 					/* (0:private 1:protected 2:unlisted 3:public)  */
+	file_content 			TEXT NOT NULL, 					/* 64KB of storage for our JSON document object */
+	file_hash			VARCHAR(255) NOT NULL,	
 	
-	PRIMARY KEY (format_id)
+	PRIMARY KEY (file_id),
+	FOREIGN KEY (file_path) 	REFERENCES Links(link_path),
 );
 
-INSERT INTO Formats (format_title, format_fontfamily, format_fontsize, format_background, format_textcolour)
-VALUES ('Default', 'Arial', 11, 'linen', 'black'); /* initialize the default values, verbose to provide template for future additions */
-
-CREATE TABLE Archives (
-	archive_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	archive_id INT NOT NULL AUTO_INCREMENT,
-	
-	/* NEEDS TO BE UPDATED TO REFLECT NEW DOCUMENT CHANGES, and change to URL_HASH unique primary key*/
-	
-	old_document_timestamp TIMESTAMP NOT NULL,
-	old_document_urlhash VARCHAR(255) NOT NULL,
-	old_document_version INT NOT NULL,
-	old_document_options VARCHAR(255) NOT NULL,
-	old_document_content TEXT NOT NULL,
-	
-	PRIMARY KEY (archive_id),
-	FOREIGN KEY (old_document_urlhash) REFERENCES Documents(document_urlhash)
-);
-
-CREATE TABLE Access (
-	access_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	access_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Permissions (
+	permission_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	permission_id 			INT NOT NULL AUTO_INCREMENT,
 	access_user_id INT NOT NULL,
 	access_document_id INT NOT NULL,
 	
@@ -92,18 +90,33 @@ CREATE TABLE Access (
 	FOREIGN KEY (access_document_id) REFERENCES Documents(document_id)
 );
 
-CREATE TABLE Actions (
-	action_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	action_id INT NOT NULL AUTO_INCREMENT,
-	action_user_id INT NOT NULL,
-	action_document_id INT NOT NULL,
+/* DRAFT - END */
+
+
+
+
+
+
+/* REFERENCE TABLES - NOT USER EDITABLE - BEGIN */
+
+CREATE TABLE Themes (
+	theme_id 			INT NOT NULL AUTO_INCREMENT,
 	
-	action_type byte NOT NULL DEFAULT 0, /* (0:create 1:delete 2:rename 3:soft_edit 4: hard_edit 5:restore 6:lock 7:unlock)  */
+	theme_title 			VARCHAR(64) NOT NULL,
+	theme_font 			VARCHAR(16) NOT NULL DEFAULT 'Arial',
+	theme_background 		VARCHAR(16) NOT NULL DEFAULT 'white', 			/* css interpretable color */
+	theme_text_color 		VARCHAR(16) NOT NULL DEFAULT 'black',
+	theme_note_color		VARCHAR(16) NOT NULL DEFAULT 'grey',
 	
-	action_information_1 VARCHAR(255) NOT NULL DEFAULT '', /* varies based on action... rename:old_title ; hard_edit:archive_id ; restore:from_version */
-	action_information_2 VARCHAR(255) NOT NULL DEFAULT '', /* varies based on action... rename:new_title ; hard_edit:current_id ; restore:to_version */
-	
-	PRIMARY KEY (action_id),
-	FOREIGN KEY (action_user_id) REFERENCES Users(user_id),
-	FOREIGN KEY (action_document_id) REFERENCES Documents(document_id)
-)
+	PRIMARY KEY (format_id)
+);
+
+INSERT INTO Themes (theme_title, theme_font, theme_background, theme_text_color, theme_note_color) VALUES ('Default', 'arial', 'white', 'black', grey);
+
+/* REFERENCE TABLES - NOT USER EDITABLE - END */
+
+
+
+
+
+
