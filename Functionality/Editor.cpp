16c2eebeb5759233eb39284d9250ed31bff48fc8
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-10-22 15:53:31 -0600

    Pseudo functionality of Editor.php JavaScript

--- Functionality/Editor.cpp
+++ Functionality/Editor.cpp
@@ -1,1 +1,17 @@
-PSEUDOCODE
+// CPP representation of what will be implemented in PHP and JavaScript
+
+#include <dos.h> //for delay()
+#include <string>
+#include <iostream>
+
+
+bool update = false;
+unsigned delayTime = 3000; //3 seconds
+string 
+
+while (true) {
+	delay(delayTime);
+	
+	
+}
+  
