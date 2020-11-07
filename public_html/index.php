58f28b8937caf8218a160dfed914817f25a30a60
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-11-07 10:40:52 -0600

    File changes, begin PHP additions

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,12 @@
-<?php session_start(); ?>
-<?php if (!(isset($_SESSION["email"]) && $_SESSION["email"])) { header("Location: login.php"); exit(); }?>
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>Ask Question</title>
-<?php include '../snippets/head.php'; ?>
-	</head>
-	<body>
-<?php include '../snippets/header.php'; ?>
-		<section>
-		</section>
-<?php include '../snippets/footer.php'; ?>
-	</body>
-</html>
+session_start();
+
+if (!(isset($_SESSION["email"]) && $_SESSION["email"])) {
+	header("Location: demo.php");
+	exit();
+} else {
+	header("Location: editor.php");
+}
+
+?>
