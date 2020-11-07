9386c52ccdcdd13c452b6c61032d157934040c08
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-11-07 12:08:27 -0600

    Update header.php

--- public_html/snippets/header.php
+++ public_html/snippets/header.php
@@ -1,35 +1,19 @@
 		<header>
-			<h1 class="title noselect">Hey! Q&amp;A</h1>
+			<div class="w3-bar w3-dark-grey noselect">
+	        <a href="demo.php"><img src="logo.png" class="w3-bar-item w3-hover-opacity" style="width:70px; height:auto;"></a>
+	        <div class="w3-bar-item bar-font-size">OutlineR</div>
+	        <a href="register.php" class="w3-bar-item bar-font-size w3-button w3-right">Register</a>
+	        <a href="login.php" class="w3-bar-item bar-font-size w3-button w3-right">Login</a>
+	    </div>
 <?php if (isset($_SESSION["email"]) && $_SESSION["email"]) { ?>
-			<div id="header_0" class="header_right">
-				<form method="post" action="../snippets/logout.php">
-					<button class="button" type="submit">Logout</button>
-				</form>
-			</div>
-			<div id="header_1" class="header_right hide_phone" style="margin-top:15px;">
-				<label id="logged_in_message" class="noselect">Welcome, <?=$_SESSION["alias"]?></label>
-			</div>
-<?php } else { ?>			
-			<div id="header_4" class="header_right hide_tiny">
-				<form method="post" action="signup.php">
-					<button class="button" type="submit">Register</button>
-				</form>
-			</div>
-			<div id="header_3" class="header_right header_mobile">
-				<form method="post" action="login.php">
-					<button class="button" type="submit">Login</button>
-				</form>
-			</div>
-			<div id="header_2" class="header_right header_desktop">
-				<form method="post" action="login.php" id="login_header">
-					<p>Email:&nbsp;</p><input size="20" name="email" type="text">
-					<p>&nbsp;</p>
-					<p>Password:&nbsp;</p><input size="20" name="pswrd" type="password">
-					<p>&nbsp;</p>
-					<button class="button" type="submit">Login</button>
-					<script type="text/javascript" src="../js/validate_login_header.js"></script>
-					<script type="text/javascript" src="../js/register_login_header.js"></script>
-				</form>
-			</div>
+
+	<a href="../snippets/logout.php" class="w3-bar-item bar-font-size w3-button w3-right">Logout</a>
+	<a href="../pages/docs.php" class="w3-bar-item bar-font-size w3-button w3-right">My Docs</a>
+
+<?php } else { ?>
+
+	<a href="../pages/register.php" class="w3-bar-item bar-font-size w3-button w3-right">Register</a>
+	<a href="../pages/login.php" class="w3-bar-item bar-font-size w3-button w3-right">Login</a>
+
 <?php	} ?>
 		</header>
