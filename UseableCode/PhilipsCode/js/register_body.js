86dafdc68cdcd659db7c7d53b3eaa5c585ed074e
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-11-07 10:28:43 -0600

    Major folder restructuring

--- UseableCode/PhilipsCode/js/register_body.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("create_post_body").addEventListener("input", validate_body, false);
-document.getElementById("create_post_send").addEventListener("click", validate_body, false);
--- UseableCode/PhilipsCode/js/toast.js
+++
@@ -1,8 +0,0 @@
-//toast.js
-
-function toast(string) {
-	var x = document.getElementById("sexy_notification");
-	x.innerHTML = string;
-	x.classList.add("show");
-	setTimeout(function(){ x.classList.remove("show"); }, 5000);
-}
