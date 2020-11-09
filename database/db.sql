49cad0ad9e8775c0418fd88aadc813d48bed1b39
Philip Ottenbreit <71906816+pkottenbreit@users.noreply.github.com>
2020-11-09 17:34:32 -0600

    Update to test version

--- database/db.sql
+++ database/db.sql
@@ -1,116 +1,64 @@
-/* IMPLEMENTED - BEGIN */
-
-/* IMPLEMENTED - END */
-
-
-
-
-
-
-/* TESTING - BEGIN */
-
 CREATE TABLE Users (
-	user_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /* creation of user account */
-	user_id 			INT NOT NULL AUTO_INCREMENT,
-
-	user_salt 			VARCHAR(255) NOT NULL, /* local .JS hash with email salt to send secure over http */
-	user_hash 			VARCHAR(255) NOT NULL, /* password, after processing */
-
-	user_alias			 VARCHAR(255) NOT NULL UNIQUE,
-	user_email 			VARCHAR(255) NOT NULL UNIQUE,
-	user_image 			VARCHAR(255) NOT NULL DEFAULT '../users/_default.png',
-
+	user_created 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP, /* creation of user account */
+	user_id 		INT NOT NULL AUTO_INCREMENT,
+	
+	user_salt 		VARCHAR(255) NOT NULL, /* local .JS hash with email salt to send secure over http */
+	user_hash 		VARCHAR(255) NOT NULL, /* password, after processing */
+	
+	user_alias		VARCHAR(255) NOT NULL UNIQUE,
+	user_email 		VARCHAR(255) NOT NULL UNIQUE,
+	user_image 		VARCHAR(255) NOT NULL DEFAULT '../users/_default.png',
+	
+	user_theme		INT NOT NULL DEFAULT 0,
+	user_font_size		INT NOT NULL DEFAULT 11,
+	
 	PRIMARY KEY (user_id)
 );
 
-/* TESTING - END */
+CREATE TABLE Docs (
+	doc_created 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	doc_id 			INT NOT NULL AUTO_INCREMENT,
+	
+	doc_path 		VARCHAR(255) NOT NULL UNIQUE,		/* try {random hash} catch(collision) {repeat} */
+	doc_creator		INT NOT NULL,
+	
+	doc_editor		INT,
+	doc_requestor		INT,
 
+	doc_title 		VARCHAR(255) NOT NULL DEFAULT '',
+	doc__content 		TEXT NOT NULL, 				/* 64KB */
+	doc__hash		VARCHAR(255) NOT NULL,
 
-
-
-
-
-/* DRAFT - BEGIN */
-
-CREATE TABLE Links (
-	link_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	link_id 			INT NOT NULL AUTO_INCREMENT,
-	link_path 			VARCHAR(255) NOT NULL UNIQUE, 			/* try {random hash} catch(collision) {repeat} */
-
-	link_owner 			INT NOT NULL,
-
-	link_hash 			VARCHAR(255) NOT NULL,
-	link_editor 			INT NOT NULL, 					/* on insert, set to owner */
-	link_version 			INT NOT NULL,
-
-	PRIMARY KEY (link_id),
-	FOREIGN KEY (link_last_lock) 	REFERENCES Users(user_id),
-	FOREIGN KEY (link_last_lock) 	REFERENCES Users(user_id),
+	PRIMARY KEY (doc__id),
+	FOREIGN KEY (doc_creator) REFERENCES Users(user_id),
+	FOREIGN KEY (doc_editor) REFERENCES Users(user_id),
+	FOREIGN KEY (doc_requestor) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Files (
-	file_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	file_id 			INT NOT NULL AUTO_INCREMENT,
-	file_path 			VARCHAR(255) NOT NULL,
-	file_parent_link		INT NOT NULL,
-
-	file_title 			VARCHAR(255) NOT NULL DEFAULT '',
-	file_privacy 			BYTE NOT NULL, 					/* (0:private 1:protected 2:unlisted 3:public)  */
-	file_content 			TEXT NOT NULL, 					/* 64KB of storage for our JSON document object */
-	file_hash			VARCHAR(255) NOT NULL,
-
-	PRIMARY KEY (file_id),
-	FOREIGN KEY (file_path) 	REFERENCES Links(link_path),
-);
-
-CREATE TABLE Permissions (
-	permission_timestamp 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	permission_id 			INT NOT NULL AUTO_INCREMENT,
-	access_user_id INT NOT NULL,
-	access_document_id INT NOT NULL,
-
-	is_owner BOOL NOT NULL DEFAULT FALSE,
-
-	edit_current BOOL NOT NULL DEFAULT FALSE,
-
-	view_actions BOOL NOT NULL DEFAULT FALSE,
-	view_archive BOOL NOT NULL DEFAULT FALSE,
-	view_current BOOL NOT NULL DEFAULT FALSE,
-
-	export_actions BOOL NOT NULL DEFAULT FALSE,
-	export_archive BOOL NOT NULL DEFAULT FALSE,
-	export_current BOOL NOT NULL DEFAULT FALSE,
-
-	change_access BOOL NOT NULL DEFAULT FALSE,
-	restore_archive	BOOL NOT NULL DEFAULT FALSE,
-	comment_current BOOL NOT NULL DEFAULT FALSE,
-
+CREATE TABLE Access (
+	access_added 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	access_id		INT NOT NULL AUTO_INCREMENT,
+	
+	access_doc		INT NOT NULL,
+	access_user		INT NOT NULL,
+	
 	PRIMARY KEY (access_id),
-	FOREIGN KEY (access_user_id) REFERENCES Users(user_id),
-	FOREIGN KEY (access_document_id) REFERENCES Documents(document_id)
-);
-
-/* DRAFT - END */
+	FOREIGN KEY (access_doc) REFERENCES Docs(doc_id),
+	FOREIGN KEY (access_user) REFERENCES Users(user_id)
+)
 
 
-
-
-
-
-/* REFERENCE TABLES - NOT USER EDITABLE - BEGIN */
 
 CREATE TABLE Themes (
-	theme_id 			INT NOT NULL AUTO_INCREMENT,
+	theme_id 		INT NOT NULL AUTO_INCREMENT,
 
-	theme_title 			VARCHAR(64) NOT NULL,
-	theme_font 			VARCHAR(16) NOT NULL DEFAULT 'Arial',
-	theme_background 		VARCHAR(16) NOT NULL DEFAULT 'white', 			/* css interpretable color */
-	theme_text_color 		VARCHAR(16) NOT NULL DEFAULT 'black',
-	theme_note_color		VARCHAR(16) NOT NULL DEFAULT 'grey',
+	theme_title 		VARCHAR(64) NOT NULL,
+	theme_font 		VARCHAR(16) NOT NULL DEFAULT 'Arial',
+	theme_background 	VARCHAR(16) NOT NULL DEFAULT 'white', 			/* css interpretable color */
+	theme_text_color 	VARCHAR(16) NOT NULL DEFAULT 'black',
+	theme_note_color	VARCHAR(16) NOT NULL DEFAULT 'grey',
 
 	PRIMARY KEY (format_id)
 );
 
 INSERT INTO Themes (theme_title, theme_font, theme_background, theme_text_color, theme_note_color) VALUES ('Default', 'arial', 'white', 'black', grey);
-
-/* REFERENCE TABLES - NOT USER EDITABLE - END */
